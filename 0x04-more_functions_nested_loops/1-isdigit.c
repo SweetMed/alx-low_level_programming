a3c8842365fd3aaf1389b49828b6e65d05b1d505
SweetMed <aminebennouh@gmail.com>
2023-02-23 05:10:20 -0800

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
 /**
  * isdigit - check for a digit (0 trough 9)
  * @c: int to be checked
- * Returns 1 if c is a digit & 0 otherwise
+ * Returns 1 if c is a digit, 0 otherwise
  */
 
 int_isdigit(int c)

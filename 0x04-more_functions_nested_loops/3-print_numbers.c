2d3a597258c3c4364002708d0c48e5eb530b032e
SweetMed <aminebennouh@gmail.com>
2023-02-23 05:55:39 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for checks for digit (0 through 9).
+ *
+ * Return: always 0.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 18; c < 28; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}

810e4098c4e4eab825f1a36de33868f2d744a691
SweetMed <aminebennouh@gmail.com>
2023-03-01 09:55:27 -0800

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,8 @@
 /**
  * string_toupper - change all lowercase to uppercase
  * @n: pointer
- * Return: char
+ *
+ * Return: n
  */
 char *string_toupper(char *n)
 {
@@ -12,7 +13,7 @@ char *string_toupper(char *n)
 	i = 0;
 	while (n[i] != '\0')
 	{
-		if (n[i] >= 'a' && n[i] <= 'Z')
+		if (n[i] >= 'a' && n[i] <= 'z')
 			n[i] = n[i] - 32;
 		i++;
 	}
